b1bb259ddb4e1b000cd88c1dede7ee8b8dc826df
Karthik Kedam <karthikmsdian78@gmail.com>
2022-10-22 23:07:05 +0530

    Time: 19 ms (85.68%), Space: 54.4 MB (15.49%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,15 +1,25 @@
 class Solution {
-    public int minEatingSpeed(int[] piles, int h) {
-     int l = 1, r = 1000000000;
-        while (l < r) {
-            int m = (l + r) / 2, total = 0;
-            for (int p : piles)
-                total += (p + m - 1) / m;
-            if (total > h)
-                l = m + 1;
-            else
-                r = m;
+     public int minEatingSpeed(int[] piles, int h) {
+        int l =1, r = 1000000000;
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            if(time(piles, mid, h) ){
+                r = mid-1;
+            }else{
+                l = mid+1;
+            }
         }
         return l;
+        
+    }
+    
+    public boolean time(int[] arr, int mid, int h  ){
+        long ans =0;
+        for(int i: arr ){
+            int div = i/mid;
+            ans += div;
+            if(i % mid != 0 ) ans++;
+        }
+        return ans <= h;
     }
 }
