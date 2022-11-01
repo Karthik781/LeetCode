3659877217ae3891c61d8d9f81234f96d3261132
Karthik Kedam <karthikmsdian78@gmail.com>
2022-11-02 02:05:39 +0530

    Time: 1 ms (94.41%), Space: 42.9 MB (28.86%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -15,28 +15,18 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        Deque<TreeNode> q = new LinkedList<>();
-        List<Integer> res = new ArrayList<>();
-        
         if(root == null) return new ArrayList<>();
-        
-        q.offerFirst(root);
-        
-        while(!q.isEmpty()){ 
-            int len = q.size();
-            res.add(q.peekLast().val);
-            while(len > 0){
-                TreeNode tmp = q.pollFirst();
-                if(tmp.left != null){
-                    q.offerLast(tmp.left);
-                }
-                if(tmp.right != null){
-                    q.offerLast(tmp.right);
-                }
-                len--;
-            }
-        }
-        
+        List<Integer> res = new ArrayList<>();
+        dfs(res, root, 0);
         return res;
+    }
+    
+    private void dfs(List<Integer> res, TreeNode root, int level){
+        if(root == null) return;
+        if(level == res.size()) 
+            res.add(0);
+        res.set(level, root.val);
+        dfs(res, root.left, level + 1);
+        dfs(res, root.right, level + 1);
     }
 }
