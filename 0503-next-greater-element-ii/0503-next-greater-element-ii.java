841462850fdbb1d0a9bda4faf5626fe9e365d8ea
Karthik Kedam <karthikmsdian78@gmail.com>
2022-11-13 00:04:19 +0530

    Time: 10 ms (95.30%), Space: 54.8 MB (53.27%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,14 +1,25 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int n = nums.length, next[] = new int[n];
-        Arrays.fill(next, -1);
-        Stack<Integer> stack = new Stack<>(); // index stack
-        for (int i = 0; i < n * 2; i++) {
-            int num = nums[i % n]; 
-            while (!stack.isEmpty() && nums[stack.peek()] < num)
-                next[stack.pop()] = num;
-            if (i < n) stack.push(i);
-        }   
-        return next;
+        // if(nums == null) return;
+        
+        Deque<Integer> dq = new LinkedList<>();
+        int len = nums.length ;
+        int[] res = new int[len];
+        
+        for(int i = 0; i < len * 2; i++){
+            int num = nums[i%len];
+            
+            while(!dq.isEmpty() && num > nums[dq.peek()]){
+                int x = dq.pop();
+                res[x] = num;
+            }
+            if(i < len) 
+                dq.push(i);
+        }
+        while(!dq.isEmpty()){
+            int x = dq.pop();
+            res[x] = -1;
+        }
+        return res;
     }
 }
