a6f6a257dccaca696ad15ebaf738923d56118566
Karthik Kedam <karthikmsdian78@gmail.com>
2022-11-15 03:00:10 +0530

    Time: 11 ms (37.92%), Space: 44.3 MB (27.71%) - LeetHub

--- 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.java
+++ 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.java
@@ -1,17 +1,12 @@
 class Solution {
     public boolean isValidSerialization(String preorder) {
-        String[] nodes = preorder.split(",");
-        Deque<String> dq = new LinkedList<>();
-        for(String s : nodes){
-            while(s.equals("#") && !dq.isEmpty() && dq.peek().equals("#")){
-                dq.pop();
-                if(dq.isEmpty()){
-                    return false;
-                }
-                dq.pop();
-            }
-            dq.push(s);
+    String[] nodes = preorder.split(",");
+    int diff = -1;
+        
+    for (String node: nodes) {
+        if (++diff > 0) return false; //inbound is 1 for all nodes(root adjusted to -1)
+        if (!node.equals("#")) diff -= 2;// outbound is 2 for all non-leafs
         }
-        return dq.size() == 1 && dq.peek().equals("#");
+    return diff == 0; 
     }
 }
