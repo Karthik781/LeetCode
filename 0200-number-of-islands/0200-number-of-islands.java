9f48c4c23b55b7ac5a6e41ea4a60ca89b825f444
Karthik Kedam <karthikmsdian78@gmail.com>
2022-11-18 02:23:55 +0530

    Time: 9 ms (50.56%), Space: 57.7 MB (28.55%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,6 +1,4 @@
 class Solution {
-    Deque<int[]> dq = new LinkedList<>();
-
     public int numIslands(char[][] grid) {
         int row = grid.length;
         int col = grid[0].length;
@@ -8,35 +6,21 @@ class Solution {
         for(int i = 0; i < row; i++){
             for(int j = 0; j < col; j++){
                 if(grid[i][j] == '1'){
-                    dq.offer(new int[]{i, j});
-                    bfs(i, j, grid);
+                    dfs(i, j, grid);
                     islands++;
                 }
             }
         }
         return islands;
     }
-    int[][] dirs = {{0,1}, {1,0}, {0, -1}, {-1, 0}};
     
-    private void bfs(int i, int j, char[][] grid){
+    private void dfs(int i, int j, char[][] grid){
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0')
+            return;
         grid[i][j] = '0';
-        int row = grid.length;
-        int col = grid[0].length;
-        
-        while(!dq.isEmpty()){
-            int[] cur = dq.poll();
-            
-            for(int[] dir : dirs){
-                int a = cur[0] + dir[0];
-                int b = cur[1] + dir[1];
-                
-                if(a >= row || b >= col || a < 0 || b < 0 || grid[a][b] == '0'){
-                    continue;
-                }
-                grid[a][b] = '0';
-                dq.offer(new int[]{a, b});
-            }
-        }
-        
+        dfs(i, j - 1, grid);
+        dfs(i - 1, j, grid);
+        dfs(i + 1, j, grid);
+        dfs(i, j + 1, grid);
     }
 }
