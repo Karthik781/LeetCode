6e26705446b0866ae96313abf051c72d61e70070
Karthik Kedam <karthikmsdian78@gmail.com>
2022-11-21 00:17:15 +0530

    Time: 11 ms (10.22%), Space: 48.7 MB (14.71%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,25 +1,39 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
+        Deque<int[]> dq = new LinkedList<>();
         int maxArea = 0;
         
-        for(int i = 0; i < grid.length; i++)  {
-            for(int j = 0; j < grid[0].length; j++){
-                if(grid[i][j] == 1){
-                    int area = dfs(i, j, grid);
+        for(int i = 0; i < grid.length; i++){
+            for(int  j = 0; j < grid[0].length; j++){
+               if(grid[i][j] == 1){
+                    dq.offer(new int[]{i, j});
+                    int area = bfs(grid, dq);
                     maxArea = Math.max(maxArea, area);
-                }
+               }
             }
         }
         return maxArea;
     }
     
-    private int dfs(int i, int j, int[][] grid){
+    int[][] dirs = new int[][]{{1,0}, {0, 1}, {-1, 0}, {0, -1}};
+    private int bfs(int[][] grid, Deque<int[]> dq){
+        int area = 1;
         
-        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0){
-            return 0;
+        while(!dq.isEmpty()){
+            int[] cur = dq.poll();
+            grid[cur[0]] [cur[1]] = 0;
+            
+            for(int[] dir : dirs){
+                int x = cur[0] + dir[0];
+                int y = cur[1] + dir[1];
+                
+                if(x >= 0 && y >= 0 && x < grid.length && y < grid[0].length && grid[x][y] == 1){
+                    dq.offer(new int[]{x, y});
+                    grid[x] [y] = 0;
+                    area++;
+                }
+            }
         }
-        grid[i][j] = 0;
-        
-        return 1 + dfs(i, j - 1, grid) + dfs(i - 1, j, grid) + dfs(i, j + 1, grid) + dfs(i+1, j, grid);
+        return area;
     }
 }
